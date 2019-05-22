63a13cdb05446618ef443f6c1fa7e360f794f165
Cale McCollough <you@example.com>
2019-05-22 10:22:59 -0700

    all.Delete Kabuki VM. #119

--- kabuki/hal/footer.inl
+++
@@ -1,2 +0,0 @@
-#include <pch.h>
-#include <kabuki/f2/seam_footer.inl>
--- kabuki/toolkit/gui/gui_common.cc
+++
@@ -1,8 +0,0 @@
-/* Kabuki Toolkit @version 0.x
-@link    https://github.com/kabuki-starship/kabuki-toolkit.git
-@file    /kabuki_toolkit/gui/gui_common.h
-@author  Cale McCollough <https://calemccollough.github.io>
-@license Copyright (C) 2014-2019 Cale McCollough <cale@astartup.net>;
-All right reserved (R). This Source Code Form is subject to the terms of the 
-Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with 
-this file, You can obtain one at https://mozilla.org/MPL/2.0/. */
